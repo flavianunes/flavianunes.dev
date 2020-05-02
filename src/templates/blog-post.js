e248061e979bd9ebaea30aefcfe428593c135323
Flavia Nunes <flavianunesufop@gmail.com>
2020-05-02 14:46:37 -0300

    project imagse

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,40 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import styles from "./styles.module.scss"
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark  
   const { previous, next } = pageContext
 
+  function createMarkup() {
+    return {__html: post.html}
+  }
+
   return (
+    <Layout>
+            <SEO title= {post.frontmatter.title}/>
+                      
+            <section>
+                <header>
+                   
+                    <h1> {post.frontmatter.title} </h1>
+                </header>
+
+
+                
+                <div className={styles.content} dangerouslySetInnerHTML={createMarkup()}></div>
+                
+            </section>
+
+            <div className={styles.gallery}>
+                  <img src={ post.frontmatter.image} />
+            </div>
+            
+        </Layout>
    
-           <h1> {post.frontmatter.title} </h1>
+           
          
 
   )
@@ -22,7 +49,9 @@ export const pageQuery = graphql`
       excerpt(pruneLength: 160)
       html
       frontmatter {
-        title
+        title,
+        image,
+        tags
         date(formatString: "MMMM DD, YYYY")
       }
     }
