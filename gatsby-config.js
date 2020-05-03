084ac57e79dd3330c888ce70ea291b32feb87bb8
Flavia Nunes <flavianunesufop@gmail.com>
2020-05-03 15:27:54 -0300

    adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: `Fluvixx.dev`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
@@ -50,13 +48,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `fluvixx.dev`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
