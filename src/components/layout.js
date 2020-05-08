b338ced33c8a0abb4c967a70bb7c7e142f85cded
Flavia Nunes <flavianunesufop@gmail.com>
2020-05-08 14:26:54 -0300

    adds structured data for seo

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import Navbar from "./navbar"
 import Footer from "./footer"
 
+
+
 const Layout = ({ children }) => {
 
   return (
--- src/components/seo.js
+++ src/components/seo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { JsonLd } from './JsonLd'
 
 
 function SEO({ title, description }) {
@@ -8,10 +9,23 @@ function SEO({ title, description }) {
     <Helmet>
       <html lang="en" />
       <meta charSet="utf-8" />
-      <title>{title} | Flavia Nunes | Frontend developer. UX design enthusiast.</title>
+      <title>{title} | Flavia Nunes | Frontend developer</title>
       <meta name="description" content={description} />
 
       <meta name="og:title" property="og:title" content={title + " | Flavia Nunes | Frontend developer. UX design enthusiast."} />
+
+      <JsonLd>
+          {{
+            '@context': 'https://schema.org',
+            '@type': 'Organization',
+            url: 'http://flavianunes.dev',
+            name: 'Flavia Nunes | Frontend developer',
+            contactPoint: {
+              '@type': 'ContactPoint',
+              email: 'flavianunesdev@gmail.com',
+            },
+          }}
+        </JsonLd>
 
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXX-X"></script>
       <script type="application/ld+json">{`
